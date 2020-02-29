a6971865f1603c60dacf372a82e1d6ceb0a5b3e5
vikamuhamedinova <vikamaha@yandex.ru>
2020-02-29 09:16:49 +0700

    Add jacoco

--- src/test/java/com/geometry/RectangleTests.java
+++ src/test/java/com/geometry/RectangleTests.java
@@ -1,22 +1,19 @@
 package com.geometry;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RectangleTests {
+public class RectangleTests {
 
     @Test
     void init() {
     }
 
     @Test
-    void perimeter() {
+    public void perimeter() {
         Rectangle rectangle = new Rectangle();
         rectangle.Init(2,3);
-        Assertions.assertEquals(rectangle.Perimeter(),10 );
+        Assertions.assertEquals(rectangle.Perimeter(),10);
     }
 
     @Test
